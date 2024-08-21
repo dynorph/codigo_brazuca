538f621670a05d4388da575fea29652dd4d9f13b
Erick Martins <erickmsilva19@gmail.com>
2024-08-21 11:41:27 -0300

    Update atividade_17.py

--- lista03_logica_python/atividade_17.py
+++ lista03_logica_python/atividade_17.py
@@ -1,7 +1,13 @@
-frase = input("Insira uma frase: ")
-contador = 1
+num = int(input("Insira um numero: "))
+lista_divisores = []
+soma = 0
 
-for char in frase:
-  if char == ' ':
-    contador += 1
-print("A frase contem", contador, "palavras")
+for i in range(1, num):
+  if num % i == 0:
+    lista_divisores.append(i)
+for i in range(len(lista_divisores)):
+  soma += lista_divisores[i]
+if soma == num:
+  print("O numero e perfeito")
+else:
+  print("O numero nao e perfeito")
