e06ea5b62e40f0733a1350e72281b998345bc24f
Erick Martins <erickmsilva19@gmail.com>
2024-08-21 11:02:36 -0300

    Create atividade_2.py

---
+++ lista02_logica_python/atividade_2.py
@@ -0,0 +1,6 @@
+celsius = float(input("Insira uma temperatura em Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
+
+print("Fahrenheit: ", fahrenheit, "graus")
+print("Kelvin: ", kelvin)
